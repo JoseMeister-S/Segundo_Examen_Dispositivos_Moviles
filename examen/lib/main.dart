cb685316ad4b290a25ae437b64d8302c83da63f1
Jose Manuel Sanchez <99229719+JoseMeister-S@users.noreply.github.com>
2024-04-02 08:31:37 -0400

    Slider for filtering

--- examen/lib/main.dart
+++ examen/lib/main.dart
@@ -8,13 +8,13 @@ void main() {
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({Key? key}) : super(key: key);
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'SEGUNDO PARCIAL Programación Móvil UCB',
-      home: PostListPage(), 
+      home: PostListPage(),
     );
   }
 }
@@ -30,6 +30,7 @@ class _PostListPageState extends State<PostListPage> {
   final ApiService _apiService = ApiService();
   List<dynamic> posts = [];
   List<double> ratings = [];
+  double thresholdRating = 3.0;
 
   @override
   void initState() {
@@ -38,8 +39,8 @@ class _PostListPageState extends State<PostListPage> {
   }
 
   Future<void> _initializeData() async {
-    await fetchPosts(); 
-    await loadRatings(); 
+    await fetchPosts();
+    await loadRatings();
   }
 
   Future<void> fetchPosts() async {
@@ -53,65 +54,101 @@ class _PostListPageState extends State<PostListPage> {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
       ratings = List<double>.generate(posts.length, (index) {
-        return prefs.getDouble('rating_$index') ?? 3.0;
+        return prefs.getDouble('rating_$index') ?? 2.5;
       });
     });
   }
 
   Future<void> saveRating(int index, double rating) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setDouble('rating_$index', rating); 
+    await prefs.setDouble('rating_$index', rating);
+  }
+
+  List<dynamic> getFilteredPosts() {
+    return List.generate(posts.length, (index) {
+      if (ratings[index] >= thresholdRating) {
+        return posts[index];
+      } else {
+        return null;
+      }
+    }).where((post) => post != null).toList();
   }
 
   @override
   Widget build(BuildContext context) {
+    List<dynamic> filteredPosts = getFilteredPosts();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Posts, Examen'),
       ),
-      body: ListView.builder(
-        itemCount: posts.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Text('Slide de filtro'),
+              Slider(
+                value: thresholdRating,
+                min: 1,
+                max: 5,
+                divisions: 8,
+                label: thresholdRating.toString(),
+                onChanged: (double value) {
+                  setState(() {
+                    thresholdRating = value;
+                  });
+                },
               ),
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('ID: ${posts[index]['id']}',
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                  SizedBox(height: 8),
-                  Text(posts[index]['title'], style: TextStyle(fontSize: 16)),
-                  SizedBox(height: 8),
-                  Text(posts[index]['body']),
-                  RatingBar.builder(
-                    initialRating: ratings[index],
-                    minRating: 1,
-                    direction: Axis.horizontal,
-                    allowHalfRating: true,
-                    itemCount: 5,
-                    itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                    itemBuilder: (context, _) => Icon(
-                      Icons.star,
-                      color: Colors.amber,
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredPosts.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    onRatingUpdate: (rating) {
-                      setState(() {
-                        ratings[index] = rating;
-                      });
-                      saveRating(index, rating); // Save the updated rating
-                    },
-                  )
-                ],
-              ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('ID: ${filteredPosts[index]['id']}',
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                        SizedBox(height: 8),
+                        Text(filteredPosts[index]['title'],
+                            style: TextStyle(fontSize: 16)),
+                        SizedBox(height: 8),
+                        Text(filteredPosts[index]['body']),
+                        RatingBar.builder(
+                          initialRating: ratings[index],
+                          minRating: 1,
+                          direction: Axis.horizontal,
+                          allowHalfRating: true,
+                          itemCount: 5,
+                          itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                          itemBuilder: (context, _) => Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                          ),
+                          onRatingUpdate: (rating) {
+                            setState(() {
+                              ratings[index] = rating;
+                            });
+                            saveRating(index, rating);
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
